fb2bfe33c3cbff35fcc560db79f5f60f2809f414
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-10-28 23:04:32 +0300

    Started to creating buying mechanic

--- src/components/shop/ShopAssortment.js
+++ src/components/shop/ShopAssortment.js
@@ -2,25 +2,38 @@ import React from 'react'
 import ShopItems from './ShopItems'
 
 
-export default function ShopAssortment() {
+export default function ShopAssortment(props) {
     const shopAssortment = [
-        {name: 'Item 1', price: 11, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 2', price: 123, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 3', price: 1444, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 4', price: 15555, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 5', price: 11111, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 6', price: 12332, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 7', price: 2332, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 8', price: 3232, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 9', price: 323232, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 10', price: 232332, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 11', price: 233232, effect: 'some_effect', image: 'somePicture.png'},
-        {name: 'Item 12', price: 2323, effect: 'some_effect', image: 'somePicture.png'},
+        {item_id: 1, name: 'Item 1', price: 11, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 2, name: 'Item 2', price: 123, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 3, name: 'Item 3', price: 1444, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 4, name: 'Item 4', price: 15555, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 5, name: 'Item 5', price: 11111, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 6, name: 'Item 6', price: 12332, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 7, name: 'Item 7', price: 2332, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 8, name: 'Item 8', price: 3232, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 9, name: 'Item 9', price: 323232, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 10, name: 'Item 10', price: 232332, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 11, name: 'Item 11', price: 233232, effect: 'some_effect', image: 'somePicture.png', available: true},
+        {item_id: 12, name: 'Item 12', price: 2323, effect: 'some_effect', image: 'somePicture.png', available: true},
     ]
+
+    const availableAssortment = shopAssortment.filter(dict => dict.available === true)
+
+    const handleUpgradeBuy = (bought_upgrade_id) => {
+        shopAssortment.map((dict) => {
+            if (dict['item_id'] === bought_upgrade_id) {
+                console.log('working')
+                dict['available'] = false
+            }
+        })
+
+    }
 
     return (
         <ShopItems 
-            shopAssortment = {shopAssortment}
+            shopAssortment = {availableAssortment}
+            handleUpgradeBuy = {handleUpgradeBuy}
         />
     )
 }
--- src/components/shop/ShopItems.js
+++ src/components/shop/ShopItems.js
@@ -2,19 +2,18 @@ import React from 'react'
 import './ShopItems.css'
 
 export default function ShopItems(props) {
-    
     return (
         props.shopAssortment.map((item) => {
-            return (
-                <div className='shop-items'>
+            return ( 
+                <div className='shop-items' key={item.item_id}>
                     <div className='image-bg'>
                         <img src={'./img/' + item.image} className='item-img'></img>
                     </div>
-                    <div key={item.name} className='item-description'>
+                    <div className='item-description'>
                         <h4>{item.name}</h4>
                         <p>Price: {item.price}$</p>
                         <p>{item.effect}</p>
-                    
+                    <button onClick={() => props.handleUpgradeBuy(item.item_id)}>Publicate</button>
                     </div>
                 </div>
                 )
