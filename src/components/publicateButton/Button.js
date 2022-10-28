c35b3d478c1d34b272a31f906d4204addc0492dc
Nikolay Ryaskov <shogo.ryaskov@gmail.com>
2022-10-28 23:01:04 +0300

    commented for future coding

--- src/components/publicateButton/Button.js
+++ src/components/publicateButton/Button.js
@@ -4,8 +4,11 @@ import './Button.css'
 export default function Button(props) {
     return (
         <section className='publicate-button'>
-            <h1>Post something!</h1>
+            <h1>Post something!</h1> 
             <button onClick={props.handleClick}>Publicate</button>
         </section>
     )
 }
+
+
+/* Post something h1 should dissapear after the first click */
