1fa243b1c3b242d032ac926ef83cc19379d3100e
FourZd <shogo.ryaskov@gmail.com>
2022-11-15 17:57:09 +0300

    sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -5,17 +5,19 @@ export default function Sidebar(){
      return (
           <nav className="sidebar">
                <NavbarBrand>
-                    <img className='logo' src='./img/Logo.png' />
+                    <h1 className='title'>Political Influencer Simulator</h1>
                </NavbarBrand>
-               <ul>Values
-                    <li>Money</li>
-                    <li>Chaotic</li>
-               </ul>
-               <ul>Menu
-                    <li>Staff image, name on hover</li>
-                    <li>Upgrades image, name on hover</li>
-                    <li></li>
-               </ul>
+               <section className='sidebar-components'>
+                    <ul>Values
+                         <li>Money</li>
+                         <li>Chaotic</li>
+                    </ul>
+                    <ul>Menu
+                         <li>Staff image, name on hover</li>
+                         <li>Upgrades image, name on hover</li>
+                         <li></li>
+                    </ul>
+               </section>
           </nav>
     )
 }
